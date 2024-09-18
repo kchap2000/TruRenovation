bd62991b754a685092b5a40a9d02eb9b38549e5a
kchap2000 <khalilcm2000@gmail.com>
2024-09-18 15:42:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/TruRenovation/',
+  root: './',  // Ensures Vite looks at the root directory for index.html
+  server: {
+    host: '0.0.0.0',
+  }
 })
