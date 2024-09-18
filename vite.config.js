7eef6c3122a249354987d5563d661184e8733159
kchap2000 <khalilcm2000@gmail.com>
2024-09-18 15:01:01 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './',  // This makes sure Vite looks at the root directory
+  root: './', // This makes sure Vite looks at the root directory
   plugins: [react()],
-  base: '/',
+  base: '/TruRenovation/', // This is important for GitHub Pages deployment
   server: {
     host: '0.0.0.0',
-  }
+  },
 })
