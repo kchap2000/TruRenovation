05507eb5bec0857069f7cc5440fe275df7bb2e53
kchap2000 <khalilcm2000@gmail.com>
2024-09-18 14:02:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: './',  // This makes sure Vite looks at the root directory
   plugins: [react()],
-  base: './',
+  base: '/',
   server: {
     host: '0.0.0.0',
-  },
-});
+  }
+})
