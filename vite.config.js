02af8b078db4e5c18820ab9f3a237bcfa06c9aa2
Khalil Chapman <khalilcm2000@gmail.com>
2024-09-19 11:22:14 -0400

    Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TruRenovation/',
-  root: './',  // Ensures Vite looks at the root directory for index.html
+  base: '/TruRenovation/', // Ensure the base path is correct for GitHub Pages
+  root: './', // This ensures Vite looks at the root directory for index.html
   server: {
     host: '0.0.0.0',
-  }
-})
+  },
+});
